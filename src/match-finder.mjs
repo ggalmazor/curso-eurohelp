3a315b1be91448848020c36853b1c7ab4b4b7af8
Guille <ggalmazor@gmail.com>
2018-10-22 19:39:02 +0200

    Generalizar la intersección usando Array.reduce - Faltaría un control de errores para menos de dos arrays

--- src/match-finder.mjs
+++ src/match-finder.mjs
@@ -4,13 +4,14 @@ import View from './view.mjs';
 const view = View();
 
 const intersectPair = criteria => (a, b) => a.filter(ax => b.some(bx => criteria(ax, bx)));
+const intersect = criteria => arrays => arrays.reduce(intersectPair(criteria));
 
 view.onReady(() => {
   api.personajes().then(view.addPersonajes);
 
   view.onBuscar(([id1, id2]) => {
     Promise.all([api.comics(id1), api.comics(id2)])
-        .then(([comics1, comics2]) => intersectPair((a, b) => a.equals(b))(comics1, comics2))
+        .then(intersect((a, b) => a.equals(b)))
         .then(view.addComics)
   });
 });
