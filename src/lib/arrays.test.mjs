183f6bd090bcf8dc6129c9add0b5acbf0b70b1ef
Guille <ggalmazor@gmail.com>
2018-10-22 20:52:12 +0200

    Nuevo test para n arrays

--- src/lib/arrays.test.mjs
+++ src/lib/arrays.test.mjs
@@ -9,3 +9,7 @@ test('Intersects a pair of arrays', () => {
   expect(intersect(eqeqeq)([[1], [1]])).toEqual([1]);
   expect(intersect(eqeqeq)([[1, 2], [2, 3]])).toEqual([2]);
 });
+
+test('Intersects an arbitrary number of arrays', () => {
+  expect(intersect(eqeqeq)([[1, 2, 3], [2, 3, 4], [3, 4, 5]])).toEqual([3]);
+});
