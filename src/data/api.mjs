5047ab70a302bd39322e14d9e062bb93ae6a0b09
Guille <ggalmazor@gmail.com>
2018-10-22 19:41:28 +0200

    Refactor del API de datos

--- src/data/api.mjs
+++ src/data/api.mjs
@@ -1,16 +1,12 @@
 import Personaje from './personaje.mjs';
 import Comic from './comic.mjs';
 
-function personajes() {
-  return fetch('/json/characters.json')
-      .then(response => response.json())
-      .then(data => data.map(Personaje.from));
-}
+const personajes = () => fetch('/json/characters.json')
+    .then(response => response.json())
+    .then(data => data.map(Personaje.from));
 
-function comics(characterId) {
-  return fetch(`/json/comics-${characterId}.json`)
-      .then(response => response.json())
-      .then(data => data.map(Comic.from));
-}
+const comics = characterId => fetch(`/json/comics-${characterId}.json`)
+    .then(response => response.json())
+    .then(data => data.map(Comic.from));
 
 export default {personajes, comics};
