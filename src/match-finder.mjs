10e61b37ff3d55a9844a693898b4818be770ba66
Guille <ggalmazor@gmail.com>
2018-10-22 19:33:36 +0200

    Extraer function. Vamos a ver si se puede generalizar

--- src/match-finder.mjs
+++ src/match-finder.mjs
@@ -3,12 +3,14 @@ import View from './view.mjs';
 
 const view = View();
 
+const interseccion = (comics1, comics2) => comics1.filter(comic1 => comics2.some(comic2 => comic1.id === comic2.id));
+
 view.onReady(() => {
   api.personajes().then(view.addPersonajes);
 
   view.onBuscar(([id1, id2]) => {
     Promise.all([api.comics(id1), api.comics(id2)])
-        .then(([comics1, comics2]) => comics1.filter(comic1 => comics2.some(comic2 => comic1.id === comic2.id)))
+        .then(([comics1, comics2]) => interseccion(comics1, comics2))
         .then(view.addComics)
   });
 });
