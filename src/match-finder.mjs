09ac20876077977171605fb1a9d36681de8bfd7d
Guille <ggalmazor@gmail.com>
2018-10-22 18:46:59 +0200

    Paso 1 completado

--- src/match-finder.mjs
+++ src/match-finder.mjs
@@ -11,6 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#buscar').addEventListener('click', event => {
     event.preventDefault();
     event.stopPropagation();
-    console.log(`P1: ${document.querySelector('#personaje1').value}, P2: ${document.querySelector('#personaje2').value}`);
+    document.querySelector('#resultados tbody').innerHTML = '';
+    const id1 = document.querySelector('#personaje1').value;
+    const id2 = document.querySelector('#personaje2').value;
+    api.comics(id1, comics1 => {
+      api.comics(id2, comics2 => {
+        for (let i = 0; i < comics1.length; i++)
+          for (let j = 0; j < comics2.length; j++)
+            if (comics1[i].id === comics2[j].id)
+              document.querySelector('#resultados tbody').innerHTML += `
+                <tr>
+                  <td>${comics1[i].id}</td>
+                  <td>${comics1[i].title}</td>
+                  <td>${comics1[i].characters}</td>
+                </tr>
+              `;
+      });
+    });
   });
 });
