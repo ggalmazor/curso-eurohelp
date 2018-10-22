38458b72bf71de2e0ebb259a8b6ca49a122cd1cc
Guille <ggalmazor@gmail.com>
2018-10-22 19:37:21 +0200

    Explicitar que la función hace la intersección de *un par* de arrays

--- src/match-finder.mjs
+++ src/match-finder.mjs
@@ -3,14 +3,14 @@ import View from './view.mjs';
 
 const view = View();
 
-const interseccion = criteria => (a, b) => a.filter(ax => b.some(bx => criteria(ax, bx)));
+const intersectPair = criteria => (a, b) => a.filter(ax => b.some(bx => criteria(ax, bx)));
 
 view.onReady(() => {
   api.personajes().then(view.addPersonajes);
 
   view.onBuscar(([id1, id2]) => {
     Promise.all([api.comics(id1), api.comics(id2)])
-        .then(([comics1, comics2]) => interseccion((a, b) => a.equals(b))(comics1, comics2))
+        .then(([comics1, comics2]) => intersectPair((a, b) => a.equals(b))(comics1, comics2))
         .then(view.addComics)
   });
 });
