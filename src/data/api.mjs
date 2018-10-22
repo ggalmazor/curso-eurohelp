b56f3a4ac54358fdf6ecfeefabeb55be3e4422eb
Guille <ggalmazor@gmail.com>
2018-10-22 19:18:21 +0200

    Migración a promesas y Promise.all

--- src/data/api.mjs
+++ src/data/api.mjs
@@ -1,18 +1,16 @@
 import Personaje from './personaje.mjs';
 import Comic from './comic.mjs';
 
-function personajes(callback) {
-  fetch('/json/characters.json')
+function personajes() {
+  return fetch('/json/characters.json')
       .then(response => response.json())
-      .then(data => data.map(Personaje.from))
-      .then(callback);
+      .then(data => data.map(Personaje.from));
 }
 
-function comics(characterId, callback) {
-  fetch(`/json/comics-${characterId}.json`)
+function comics(characterId) {
+  return fetch(`/json/comics-${characterId}.json`)
       .then(response => response.json())
-      .then(data => data.map(Comic.from))
-      .then(callback);
+      .then(data => data.map(Comic.from));
 }
 
 export default {personajes, comics};
--- src/match-finder.mjs
+++ src/match-finder.mjs
@@ -4,16 +4,15 @@ import View from './view.mjs';
 const view = View();
 
 view.onReady(() => {
-  api.personajes(view.addPersonajes);
+  api.personajes().then(view.addPersonajes);
 
   view.onBuscar(([id1, id2]) => {
-    api.comics(id1, comics1 => {
-      api.comics(id2, comics2 => {
-        for (let i = 0; i < comics1.length; i++)
-          for (let j = 0; j < comics2.length; j++)
-            if (comics1[i].id === comics2[j].id)
-              view.addComic(comics1[i]);
-      });
-    });
+    Promise.all([api.comics(id1), api.comics(id2)])
+        .then(([comics1, comics2]) => {
+          for (let i = 0; i < comics1.length; i++)
+            for (let j = 0; j < comics2.length; j++)
+              if (comics1[i].id === comics2[j].id)
+                view.addComic(comics1[i]);
+        });
   });
 });
