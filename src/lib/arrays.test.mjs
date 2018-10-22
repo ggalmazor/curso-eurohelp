a64c8cef1e10b69c228624ef6f3d2c4021b19e27
Guille <ggalmazor@gmail.com>
2018-10-22 20:51:23 +0200

    Mejora del primer test con más casos

--- src/lib/arrays.test.mjs
+++ src/lib/arrays.test.mjs
@@ -2,6 +2,10 @@ const {intersect} = require("./arrays.mjs");
 
 const eqeqeq = (a, b) => a === b;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(intersect(eqeqeq)([], [])).toEqual([]);
+test('Intersects a pair of arrays', () => {
+  expect(intersect(eqeqeq)([[], []])).toEqual([]);
+  expect(intersect(eqeqeq)([[1], []])).toEqual([]);
+  expect(intersect(eqeqeq)([[], [1]])).toEqual([]);
+  expect(intersect(eqeqeq)([[1], [1]])).toEqual([1]);
+  expect(intersect(eqeqeq)([[1, 2], [2, 3]])).toEqual([2]);
 });
