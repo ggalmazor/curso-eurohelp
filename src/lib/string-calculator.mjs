c426a9290dd7a19167812941c4a7f7b93bfaef12
Guille <ggalmazor@gmail.com>
2018-10-27 12:41:03 +0200

    Refactor: extraer una función normalize(input)

--- src/lib/string-calculator.mjs
+++ src/lib/string-calculator.mjs
@@ -1,15 +1,20 @@
 const replaceAll = (replacement, search, text) => text.replace(new RegExp(search, "g"), replacement);
 
-export default input => {
-  if (input === "")
-    return 0;
-
+const normalize = input => {
   let normalizedInput = replaceAll(",", "\n", input);
   if (normalizedInput.startsWith("//")) {
     const separator = normalizedInput[2];
     const body = normalizedInput.substring(4);
     normalizedInput = replaceAll(",", separator, body);
   }
+  return normalizedInput;
+};
+
+export default input => {
+  if (input === "")
+    return 0;
+
+  const normalizedInput = normalize(input);
 
   const parts = normalizedInput.split(",");
   const numbers = parts.map(part => parseInt(part, 10));
