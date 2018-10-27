a6b90b8dfb89adfff839918f6544eabfebca39b2
Guille <ggalmazor@gmail.com>
2018-10-27 12:27:06 +0200

    Refactor para ver mejor la estructura del código

--- src/lib/string-calculator.mjs
+++ src/lib/string-calculator.mjs
@@ -5,5 +5,7 @@ export default input => {
   if (input.indexOf(",") === -1)
     return parseInt(input, 10);
 
-  return parseInt(input.substring(0, input.indexOf(","))) + parseInt(input.substring(input.indexOf(",") + 1));
+  const partA = input.substring(0, input.indexOf(","));
+  const partB = input.substring(input.indexOf(",") + 1);
+  return parseInt(partA) + parseInt(partB);
 }
