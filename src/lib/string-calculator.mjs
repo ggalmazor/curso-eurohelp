b014118ad8ced0d912024183200724cd45f141da
Guille <ggalmazor@gmail.com>
2018-10-27 12:22:21 +0200

    Test en verde "1,2" = 3

--- src/lib/string-calculator.mjs
+++ src/lib/string-calculator.mjs
@@ -2,5 +2,8 @@ export default input => {
   if (input === "")
     return 0;
 
-  return parseInt(input, 10);
+  if (input.indexOf(",") === -1)
+    return parseInt(input, 10);
+
+  return parseInt(input[0]) + parseInt(input[2]);
 }
