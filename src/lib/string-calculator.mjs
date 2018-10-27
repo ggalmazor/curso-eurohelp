fb631a6b51f2c94916410846e8c39a024a859e5b
Guille <ggalmazor@gmail.com>
2018-10-27 12:34:11 +0200

    Test en verde "1\\n2" = 3

--- src/lib/string-calculator.mjs
+++ src/lib/string-calculator.mjs
@@ -2,10 +2,12 @@ export default input => {
   if (input === "")
     return 0;
 
-  if (input.indexOf(",") === -1)
-    return parseInt(input, 10);
+  const normalizedInput = input.replace(/\n/g, ",");
 
-  const parts = input.split(",");
+  if (normalizedInput.indexOf(",") === -1)
+    return parseInt(normalizedInput, 10);
+
+  const parts = normalizedInput.split(",");
   const numbers = parts.map(part => parseInt(part, 10));
   return numbers.reduce((a, b) => a + b, 0);
 }
