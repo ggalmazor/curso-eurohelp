ce788867f7e217cc62687ac2c8ed1a24f4c6f757
Guille <ggalmazor@gmail.com>
2018-10-27 12:35:00 +0200

    Refactor para simplificar el código

--- src/lib/string-calculator.mjs
+++ src/lib/string-calculator.mjs
@@ -4,9 +4,6 @@ export default input => {
 
   const normalizedInput = input.replace(/\n/g, ",");
 
-  if (normalizedInput.indexOf(",") === -1)
-    return parseInt(normalizedInput, 10);
-
   const parts = normalizedInput.split(",");
   const numbers = parts.map(part => parseInt(part, 10));
   return numbers.reduce((a, b) => a + b, 0);
