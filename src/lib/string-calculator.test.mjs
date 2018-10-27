bb6c7e143de525d55bf8ae3f88b597c920b849d2
Guille <ggalmazor@gmail.com>
2018-10-27 12:48:33 +0200

    Refactor del test para darle un poco de estructura al behavior

--- src/lib/string-calculator.test.mjs
+++ src/lib/string-calculator.test.mjs
@@ -9,16 +9,22 @@ test("Puede sumar numeros separados con coma dentro de una cadena", () => {
   expect(sc("1,2,3")).toBe(6);
 });
 
-test("Puede usar otros separadores", () => {
-  expect(sc("1\n2")).toBe(3);
-  expect(sc("//;\n1;2")).toBe(3);
+describe("Puede usar otros separadores", () => {
+  test("Admite el carácter de nueva línea como separador", () => {
+    expect(sc("1\n2")).toBe(3);
+  });
+  test("Permite definir separadores personalizados", () => {
+    expect(sc("//;\n1;2")).toBe(3);
+  });
 });
 
-test("Falla si detecta números negativos", () => {
-  expect(() => sc("-1,2")).toThrow(/-1/);
-});
+describe("Reglas especiales sobre números", () => {
+  test("Falla si detecta números negativos", () => {
+    expect(() => sc("-1,2")).toThrow(/-1/);
+  });
 
-test("Ignora números mayores o iguales a 1000", () => {
-  expect(sc("1000,2")).toBe(2);
+  test("Ignora números mayores o iguales a 1000", () => {
+    expect(sc("1000,2")).toBe(2);
+  });
 });
 
